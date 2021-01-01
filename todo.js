a99af41f2853120d443713c69f5eae73efdb2ddb
Kovacs Laszlo Zsolt <laszlo.zs.kovacs@gmail.com>
2021-01-02 00:15:49 +0100

    add getList function

--- todo.js
+++ todo.js
@@ -1,4 +1,6 @@
 import minimist from 'minimist';
+import fs, { write, writeFile, writeFileSync } from 'fs'
+import { getMaxListeners } from 'process';
 
 const args = minimist(process.argv);
 
@@ -14,7 +16,18 @@ Parancssori argumentumok:
         console.log(userManual);
     }
 
+function getList() {
+    let todoList = fs.readFileSync('./todoList.txt', 'utf-8').split('\n');
+    for (let i = 0; i < todoList.length; i++) {
+        console.log(`${i + 1}. ${todoList[i]}`);
+    }
+}
+
 // run without args and print userguide
-if (args[0] === undefined) {
+if (Object.keys(args).length < 2) {
     userGuide();
+}
+
+if (args.l === true) {
+    getList();
 }
