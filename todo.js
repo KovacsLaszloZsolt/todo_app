811685b4d399f1e6dde4250abc82078536bc4bf2
Kovacs Laszlo Zsolt <laszlo.zs.kovacs@gmail.com>
2021-01-02 01:25:10 +0100

    added removeTask function

--- todo.js
+++ todo.js
@@ -2,7 +2,7 @@ import minimist from 'minimist';
 import fs from 'fs';
 
 const args = minimist(process.argv);
-let todoList = fs.readFileSync('./todoList.txt', 'utf-8');
+
 function userGuide() {
         const userManual = `Parancssori Todo applikáció
 =============================
@@ -15,13 +15,17 @@ Parancssori argumentumok:
         console.log(userManual);
     }
 
-function getList(todoList) {
+function getList() {
+
+    let todoList = fs.readFileSync('./todoList.txt', 'utf-8');
+
     if (todoList.length === 0) {
         console.log('Nincs mára tennivalód! :)');
         return;
     }
-
+    
     todoList = todoList.split('\n');
+
     for (let i = 0; i < todoList.length; i++) {
         
         console.log(`${i + 1}. ${todoList[i]}`);
@@ -30,7 +34,19 @@ function getList(todoList) {
 
 function addNewTask(args) {
 
-    fs.appendFileSync('./todoList.txt', '\n' + args, 'utf-8');
+    fs.appendFileSync('./todoList.txt', '\n' + args);
+}
+
+function removeTask(index) {
+
+    let todoList = fs.readFileSync('./todoList.txt', 'utf-8').split('\n');
+    todoList.splice(index, 1);
+
+    fs.writeFileSync('./todoList.txt', '');
+
+    for (let i = 0; i < todoList.length; i++) {
+        fs.appendFileSync('./todoList.txt', todoList[i] + '\n');
+    }
 }
 
 // run without args and print userguide
@@ -39,11 +55,15 @@ if (Object.keys(args).length < 2) {
 }
 
 if (args.l === true) {
-    getList(todoList);
+    getList();
 }
 
 if (args.a === 'string') {
     addNewTask(args.a);
 } else if (args.a === true){
     console.log('Nem lehetséges új feladat hozzáadása: nincs megadva a feladat!');
+}
+
+if (typeof(args.r) === 'number') {
+    removeTask(args.r);
 }
